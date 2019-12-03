49e21187be9adf86cf56be4ca067efb2eb4291e1
hannut91 <hannut91@gmail.com>
2019-12-03 20:30:51 +0900

    Add Lecture 10
    Add Dependency Injection of RestaurantRepository.

---
+++ eatgo-api/src/main/kotlin/kr/co/fastcampus/eatgo/domain/RestaurantRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package kr.co.fastcampus.eatgo.domain
+
+import org.springframework.stereotype.Component
+
+@Component
+class RestaurantRepositoryImpl : RestaurantRepository {
+    private val restaurants = arrayListOf<Restaurant>()
+
+    constructor() {
+        restaurants.add(Restaurant(1004, "Bob zip", "Seoul"))
+        restaurants.add(Restaurant(2020, "Cyber food", "Seoul"))
+    }
+
+    override fun findAll(): List<Restaurant> {
+        return restaurants
+    }
+
+    override fun findById(id: Long): Restaurant? {
+        return restaurants.firstOrNull { it.id.equals(id) }
+    }
+}
